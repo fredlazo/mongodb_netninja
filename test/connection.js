93109ec5ae8502547f20de66867b17ce7da9092e
Frederick Lazo <fredlazo@yahoo.com>
2017-03-04 14:17:52 -0800

    Finished Lesson 3

---
+++ test/connection.js
@@ -0,0 +1,20 @@
+//The folder is called test because we'll test using Mocha later
+//This file contains all of our code for connecting to the database
+
+//Require mongoose
+const mongoose = require('mongoose');
+
+//Connect to mongodb
+//This is where you say where you want to connect to
+//localhost because we installed it locally
+//mongoose will create dbs even it they don't exist yet (testaroo)
+mongoose.connect('mongodb://localhost/testaroo');
+
+//App doesn't know when the db is ready
+//.once is an event listener equivalent to .on
+//.
+mongoose.connection.once('open', function(){
+    console.log('Connection has been made...');
+}).on('error', function(error){
+    console.log('Connection error', error);
+});
