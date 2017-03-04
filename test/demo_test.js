58e5db9cae074ba728761e441e79fd83c189a89a
Frederick Lazo <fredlazo@yahoo.com>
2017-03-04 15:14:24 -0800

    Lesson 5 completed

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+//You don't need to require mocha.  It will still work.
+//const mocha = require('mocha');
+const assert = require('assert');
+
+describe('Just demo tests', function(){
+    // Create tests
+    it('adds two numbers together', function(){
+        assert(2+3 === 5);
+    });
+});
