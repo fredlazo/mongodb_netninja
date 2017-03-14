3205a35686c456e9adbd042b949bce3252af00eb
Frederick Lazo <fredlazo@yahoo.com>
2017-03-14 15:02:44 -0700

    Lesson 11 completed

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Don't use Homebrew
 CONNECTION:
 Make sure dir folder has read AND write priveleges
 
-$ ps aux | grep mongo
+ps aux | grep mongo
 $ kill <insert number here>
--- test/finding_test.js
+++ test/finding_test.js
@@ -8,10 +8,19 @@ const MarioChar = require('../models/mariochar');
 describe('Finding records', function(){
     //This is run  BEFORE EACH test
     //so we actually have something to find.
+
+    //char is declared here because because
+    //it's defined in beforeEach, and you don't have
+    //access outside of beforeEach
+    var char;
+
     beforeEach(function(done){
-        var char = new MarioChar({
+        //This var char is no longer declared here...
+        char = new MarioChar({
             name: 'Mario',
             weight: '500'
+            //mongoose automatically creates an ID
+
         });
         char.save().then(function(){
             assert(char.isNew === false); //This can be removed
@@ -24,6 +33,17 @@ describe('Finding records', function(){
         //.then fires after it finds something.
         MarioChar.findOne({name: 'Mario'}).then(function(result){
             assert(result.name === 'Mario');
+            done();
+        })
+    });
+
+
+    it('Finds one record by ID from the database', function(done){
+        //.then fires after it finds something.
+        //Don't need .toString.  Mongoose knows.
+        MarioChar.findOne({_id: char._id}).then(function(result){
+            //But we need .toString here
+            assert(result._id.toString() === char._id.toString());
             done();
         })
     });
