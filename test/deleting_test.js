cb72e659f0a618537083ce74bd14073bc383f892
Frederick Lazo <fredlazo@yahoo.com>
2017-03-14 18:12:02 -0700

    Lesson 12 completed

---
+++ test/deleting_test.js
@@ -0,0 +1,40 @@
+
+
+//You don't need to require mocha.  It will still work.
+//const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+describe('Deleting records', function(){
+    //This is run  BEFORE EACH test
+    //so we actually have something to delete.
+
+    //char is declared here because because
+    //it's defined in beforeEach, and you don't have
+    //access outside of beforeEach
+    var char;
+
+    beforeEach(function(done){
+        //This var char is no longer declared here...
+        char = new MarioChar({
+            name: 'Mario',
+            weight: '500'
+            //mongoose automatically creates an ID
+        });
+        char.save().then(function(){
+            assert(char.isNew === false); //This can be removed
+            done(); //Okay, it's done.  We can test now.
+        });
+    });
+
+    // Create tests
+    it('Deletes one record from the database', function(done){
+        //.then fires after it finds something.
+        MarioChar.findOneAndRemove({name: 'Mario'}).then(function(){
+            MarioChar.findOne({name: 'Mario'}).then(function(result){
+                assert(result === null);
+                done();
+            });
+        });
+    });
+});
